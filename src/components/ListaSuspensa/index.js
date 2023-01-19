a0f66631940eecc47732c569bdc90ef7eb593755
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-19 16:32:01 -0300

    Corrigindo bugs

--- src/components/ListaSuspensa/index.js
+++ src/components/ListaSuspensa/index.js
@@ -1,15 +1,17 @@
 import './ListaSuspensa.css';
 
 const ListaSuspensa = (props) => {
-  const {label, itens, obrigatorio} = props;
+  const {label, itens, obrigatorio, valor} = props;
 
   return (
     <div className='lista-suspensa'>
       <label>{label}</label>
       <select 
         required={obrigatorio}
-        onChange={event => props.aoAlterar(event.target.value)} 
+        onChange={event => props.aoAlterar(event.target.value)}
+        value={valor} 
       >
+        <option value=""></option>
         {itens.map(item => <option key={item} value={item}>{item}</option>)}
       </select>
     </div>
